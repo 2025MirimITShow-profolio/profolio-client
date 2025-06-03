5765195ee39184210d207fe5e1e085dfeed4c47e
juyoung07 <yunju5090@gmail.com>
2025-06-03 21:45:57 +0900

    [feat] : tailwind css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Portfolio from './pages/Portfolio'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="portfolio" element={<Portfolio/>} />
-      </Routes>
-    </Router>
-  )
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+				<Route path="portfolio" element={<Portfolio />} />
+			</Routes>
+		</Router>
+	);
 }
 
-export default App
+export default App;
