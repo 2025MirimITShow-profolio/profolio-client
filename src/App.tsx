af1524c1930afe8dd3df54731630a392c2aaffbe
leesori1410 <w2319@e-mirim.hs.kr>
2025-06-14 23:27:23 +0900

    [fix] : 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
 import Main from "./pages/Main";
-import Dashboard from "./pages/Dashboard";
-import Portfolio from "./pages/Portfolio";
 import Member from "./pages/Member";
 import ProjectIntroduce from "./pages/ProjectIntroduce";
-import Todo from "./pages/Todo";
-import ProjectDescription from "./pages/ProjectDescription";
 
 function App() {
 	return (
@@ -16,9 +12,6 @@ function App() {
 				<Route path="/profolio" element={<Main />} />
 				<Route path="/introduce" element={<ProjectIntroduce />} />
         		<Route path="/member" element={<Member />} />
-				<Route path="portfolio" element={<Portfolio />} />
-				<Route path="/description" element={<ProjectDescription />} />
-				<Route path="/todo" element={<Todo />} />
 			</Routes>
 		</Router>
 	);
