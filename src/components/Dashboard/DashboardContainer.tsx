62044e161d4f0c981ef68512aa31dd2afce895c0
leesori1410 <w2319@e-mirim.hs.kr>
2025-06-18 12:05:28 +0900

    [feat] : project, task 진행도 api 연동

--- src/components/Dashboard/DashboardContainer.tsx
+++ src/components/Dashboard/DashboardContainer.tsx
@@ -4,42 +4,89 @@ import Members from './Members'
 import DailyGoals from './DailyGoals'
 import ProjectTimeline from './ProjectTimeline'
 import styles from '../../style/DashboardContainer.module.css'
+import axios from 'axios'
+import { useUserStore } from '../../store/userStore'
+import { useEffect, useState } from 'react'
 
 const simpleItem = [
     {
         title: '진행 중인 프로젝트',
         src: 'ongoingProject',
-        count: 3,
+        count: 0,
         color: '#0070FF',
     },
     {
         title: '완료한 프로젝트',
         src: 'projectsCompleted',
-        count: 6,
+        count: 0,
         color: '#03D7C2',
     },
     {
         title: '해야 하는 일',
         src: 'todo',
-        count: 2,
+        count: 0,
         color: '#9385F7',
     },
     {
         title: '완료한 일',
         src: 'done',
-        count: 1,
+        count: 0,
         color: '#FE996B',
     },
 ]
 
 export default function DashboardContainer() {
+    const token = useUserStore((store) => store.token)
+    const [simpleBox, setSimpleBox] = useState(simpleItem)
+
+    const getProjectCount = async() => {
+        try {
+            const res = await axios.get('http://localhost:3000/api/projects/counts', {
+                headers : {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            const data = res.data
+            const updatedBox = [...simpleBox]
+            updatedBox[0].count = data.in_progress
+            updatedBox[1].count = data.completed
+            setSimpleBox(updatedBox)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const getTaskCount = async() => {
+        try {
+            const res = await axios.get('http://localhost:3000/api/tasks/counts', {
+                headers : {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            const data = res.data
+            const updatedBox = [...simpleBox]
+            updatedBox[2].count = data.in_progress
+            updatedBox[3].count = data.completed
+            setSimpleBox(updatedBox)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getProjectCount()
+        getTaskCount()
+    }, [])
+    useEffect(() => {
+        console.log(simpleBox);
+    }, [simpleBox])
+
     return (
         <div className={styles.dashboardContainer}>
             <div 
                 className={styles.container} 
             >
                 <div className={styles.flexContainer}>
-                    {simpleItem.map((item, i) => (
+                    {simpleBox.map((item, i) => (
                         <SimpleBox key={i} item={item} />
                     ))}
                 </div>
